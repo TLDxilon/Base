6a2bd3acbaca5fe050a1b031be2bb071316218d0
toya <toya.dapena@gmail.com>
2018-05-18 13:09:50 +0200

    dev

--- js/core.js
+++ js/core.js
@@ -1,7 +1,24 @@
 $(document).ready(function($) {
 
+    $('.brand-js .dropdown-toggle').click(function() {
+        $('.brand-js .dropdown-menu').toggle('show');
+    });
 
-    $('.alert').alert();
-    $().alert('close');
+    $('.range-js .dropdown-toggle').click(function() {
+        $('.range-js .dropdown-menu').toggle('show');
+    });
+
+    $('.size-js .dropdown-toggle').click(function() {
+        $('.size-js .dropdown-menu').toggle('show');
+    });
+    $('.color-js .dropdown-toggle').click(function() {
+        $('.color-js .dropdown-menu').toggle('show');
+    });
+
+    $('.dropdown-close-filters-js a').click(function() {
+        $(this).parents('.dropdown-menu').css('display', 'none');
+    });
+
+
 
 });
